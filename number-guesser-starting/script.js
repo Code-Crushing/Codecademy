da70b1de86cf76973ea404be3cae3e891f2478ee
Fahd-Elouardichi <elouardichifahd1998@gmail.com>
2023-05-28 03:57:08 +0100

    JS-PR:NumberGuesser

--- number-guesser-starting/script.js
+++ number-guesser-starting/script.js
@@ -4,3 +4,32 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10)
+}
+const compareGuesses = (user, computer, target) => {
+    target = generateTarget();
+    if (computer === target) {
+        return 'Computer Won!';
+    }
+    else if ((computer > target) && (user > computer)){
+        return 'Computer Won!';
+    }
+    else if ((computer < target) && (user < computer)){
+        return 'Computer Won!';
+    }
+    else {
+        return 'You Win!'
+    }
+}
+const updateScore = (winner) => {
+    if (winner === 'human') {
+        humanScore++;
+    }
+    else if (winner === 'computer') {
+        computerScore++;
+    }
+}
+const advanceRound = () => {
+    currentRoundNumber++;
+}
