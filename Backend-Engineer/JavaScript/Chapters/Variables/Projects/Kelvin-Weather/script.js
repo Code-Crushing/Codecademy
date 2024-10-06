30a7a1af07b5b4e6805782188bcdcb7b7e31fd72
CodeCruser <elouardichifahd1998@gmail.com>
2024-10-06 01:27:42 +0100

    Kelvin Weather project finished

--- Backend-Engineer/JavaScript/Chapters/Variables/Projects/Kelvin-Weather/script.js
+++ Backend-Engineer/JavaScript/Chapters/Variables/Projects/Kelvin-Weather/script.js
@@ -1,4 +1,19 @@
 // This will  be a constant variable
 const kelvin = 293;
+
+// Celsiue is different from Kelvin
 const celsius = kelvin - 273;
 
+// an equation to calculat fahrenheit
+let fahrenheit = celsius * (9/5) + 32
+
+// This will round up the result to an integer instead of a decimal
+fahrenheit = Math.floor(fahrenheit)
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
+
+// this equation will help converting from celsius to Newton
+let newton = celsius * (33/100);
+newton = Math.floor(newton);
+
+console.log(`The temperature is ${newton} degrees Newton`);
