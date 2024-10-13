c8cf123e1917c0d7cf592cbc3d8dae0aa563ef16
CodeCruser <elouardichifahd1998@gmail.com>
2024-10-13 03:11:12 +0100

    project Magic Eight Ball finished

--- Backend-Engineer/JavaScript/Chapters/Conditiona-Statements/Projects/Magic-Eight-Ball/Magic-Eight-Ball.js
+++ Backend-Engineer/JavaScript/Chapters/Conditiona-Statements/Projects/Magic-Eight-Ball/Magic-Eight-Ball.js
@@ -3,3 +3,41 @@ let userName = 'Fahd';
 // a ternary expression that decides what to do if the user enters a name or not. If the user enters a name
 
 userName? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+
+const userQuestion = 'Am I gonna continue the journey?';
+console.log(`${userName} asked: ${userQuestion}`);
+
+let randomeNumber = Math.floor(Math.random() *8);
+
+let eightBall = '';
+
+switch (randomeNumber){
+    case 0:
+        eightBall = 'It is certain';
+        break;
+    case 1:
+        eightBall = 'Maybe';
+        break;
+    case 2:
+        eightBall = 'It is decidedly so';
+        break;
+    case 3:
+        eightBall = 'Reply hazy try again';
+        break;
+    case 4:
+        eightBall = 'Cannot predict now';
+        break;
+    case 5:
+        eightBall = 'Do not count on it';
+        break;
+    case 6:
+        eightBall = 'My sources say no';
+        break;
+    case 7:
+        eightBall = 'Outlook not so good';
+        break;
+    case 8:
+        eightBall = 'Signs point to yes';
+        break;
+}
+console.log(eightBall);
