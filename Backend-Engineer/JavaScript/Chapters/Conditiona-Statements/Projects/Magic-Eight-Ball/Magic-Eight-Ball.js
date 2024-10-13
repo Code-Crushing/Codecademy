08b94d16b933f75ec3856f9e023acbb7c19a8965
CodeCruser <elouardichifahd1998@gmail.com>
2024-10-13 02:54:47 +0100

    working on conditional statments project Magic Eight Ball

---
+++ Backend-Engineer/JavaScript/Chapters/Conditiona-Statements/Projects/Magic-Eight-Ball/Magic-Eight-Ball.js
@@ -0,0 +1,5 @@
+let userName = 'Fahd';
+
+// a ternary expression that decides what to do if the user enters a name or not. If the user enters a name
+
+userName? console.log(`Hello, ${userName}!`) : console.log('Hello!');
