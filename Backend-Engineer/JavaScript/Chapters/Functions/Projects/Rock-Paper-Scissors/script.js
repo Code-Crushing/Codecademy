d949a87d6c442af3b0319f1dc01e272db3c19eb3
CodeCruser <elouardichifahd1998@gmail.com>
2024-10-14 19:47:55 +0100

    Rock Paper Scissors project finished

--- Backend-Engineer/JavaScript/Chapters/Functions/Projects/Rock-Paper-Scissors/script.js
+++ Backend-Engineer/JavaScript/Chapters/Functions/Projects/Rock-Paper-Scissors/script.js
@@ -1,5 +1,61 @@
 console.log('hi');
 
-const getUserChoice = (useInput) =>{
-    
+const getUserChoice = (userInput) =>{
+    userInput = userInput.toLowerCase();
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors'){
+        return userInput;
+    } else {
+        console.log('Not a proper choice, try again!');
+    }
 }
+// console.log(getUserChoice('Rock'));
+
+const getComputerChoice = () =>{
+    let number = Math.floor(Math.random() * 3);
+    switch(number){
+        case 0: 
+            return 'rock';
+            break;
+
+        case 1:
+            return 'paper';
+            break;
+        
+        case 2:
+            return 'scissors';
+            break;
+    }
+}
+// console.log(getComputerChoice());
+
+const determineWinner = (userChoice, computerChoice) => {
+    if (userChoice === computerChoice){
+        return 'It is a tie'
+    }
+
+    if (userChoice === 'rock') {
+        
+        if(computerChoice === 'paper'){
+            return "You lost the computer won";
+        } else {
+            return "You won!"
+        }
+    }
+    if (userChoice === 'paper') {
+
+        if(computerChoice === 'scissors'){
+            return "You lost the computer won";
+        } else {
+            return "You won!"
+        }
+    }
+    if (userChoice === 'scissors') {
+
+        if(computerChoice === 'rock'){
+            return "You lost the computer won";
+        } else {
+            return "You won!"
+        }
+    }
+}
+console.log(determineWinner(getUserChoice('rock'), getComputerChoice()));
