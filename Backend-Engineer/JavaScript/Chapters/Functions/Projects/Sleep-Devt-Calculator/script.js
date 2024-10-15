299b5c120ad73d703e79c93f174b27061dbcfc56
CodeCruser <elouardichifahd1998@gmail.com>
2024-10-15 04:41:08 +0100

    sleep debt calculator project finished

--- Backend-Engineer/JavaScript/Chapters/Functions/Projects/Sleep-Devt-Calculator/script.js
+++ Backend-Engineer/JavaScript/Chapters/Functions/Projects/Sleep-Devt-Calculator/script.js
@@ -20,9 +20,34 @@ const getSleepHours = (day)=>{
             return 4;
             break;
         case 'sunday':
-            return 9;
+            return 11;
             break;
     }
 }
 
-console
+// console.log(getSleepHours('sunday'));
+
+const getActualSleepHours = () => getSleepHours('monday')+ getSleepHours('tuesday')+ getSleepHours('wednesday')+ getSleepHours('thursday')+ getSleepHours('friday')+ getSleepHours('saturday')+ getSleepHours('sunday');
+
+// console.log(getActualSleepHours());
+
+const getIdealSleepHours = () =>{
+    let idealHours = 7;
+    return idealHours *= 7;
+}
+// console.log(getIdealSleepHours());
+
+const calculateSleepDeebt = () =>{
+    let actualSleepHours = getActualSleepHours();
+    let idealSleepHours = getIdealSleepHours();
+
+    if(actualSleepHours === idealSleepHours){
+        console.log('You got an ideal week of sleep');
+    } else if (actualSleepHours < idealSleepHours){
+        console.log('You are lacking a few hours of sleep try to get your self more composed')
+    } else {
+        console.log('You got more sleep then intended good for you!')
+    }
+}
+console.log(calculateSleepDeebt());
+
