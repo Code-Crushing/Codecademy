9751103e75c30940569f41b17584886e9b3c9a62
CodeCruser <elouardichifahd1998@gmail.com>
2024-10-25 23:30:46 +0100

    tets

--- Backend-Engineer/JavaScript/Projects/Number-Guesser/script.js
+++ Backend-Engineer/JavaScript/Projects/Number-Guesser/script.js
@@ -4,3 +4,30 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () =>{
+    return Math.floor(Math.random() * 10);
+}
+// testing the function generateTarget()
+console.log(generateTarget());
+
+const compareGuesses = (randomGuess, userGuess, computerGuess) =>{
+    let 
+    if(userGuess === computerGuess){
+        return true;
+    } else if (randomGuess - userGuess < randomGuess - computerGuess){
+        return `${true} case of user input is near the target`;
+    
+    } else if (randomGuess - userGuess > randomGuess - computerGuess){
+        return false;
+    
+    } else if (randomGuess - userGuess >= randomGuess - computerGuess){
+        return `${false}  case of equality`;
+    }
+}
+
+// testing the function generateTarget()
+console.log(compareGuesses(generateTarget(), 4, 5));
+console.log(compareGuesses(generateTarget(), 8, 4));
+console.log(compareGuesses(5, 4, 4));
+
+
