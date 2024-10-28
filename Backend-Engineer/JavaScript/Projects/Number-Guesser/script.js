c7ec879d82682862ab7ad88d6936c0c280e20f03
CodeCruser <elouardichifahd1998@gmail.com>
2024-10-28 06:00:31 +0100

    needs adjustment for the interface regarding the computerGuess value th iput is not similiar in the interface for the cmpareGuesserfunction

--- Backend-Engineer/JavaScript/Projects/Number-Guesser/script.js
+++ Backend-Engineer/JavaScript/Projects/Number-Guesser/script.js
@@ -8,29 +8,61 @@ const generateTarget = () =>{
     return Math.floor(Math.random() * 10);
 }
 // testing the function generateTarget()
-console.log(generateTarget());
+console.log(generateTarget() + ' target');
 
-const compareGuesses = (randomGuess, userGuess, computerGuess) =>{
-    let userDivid = randomGuess - userGuess;
-    let computerDivid = randomGuess - computerGuess;
-    userDivid = Math.abs(userDivid);
-    computerDivid = Math.abs(computerDivid);
-    if(userGuess === computerGuess){
-        return true;
-    } else if ( userDivid < computerDivid){
-        return `${true} case of user input is near the target`;
-    
-    } else if (userDivid > computerDivid){
-        return false;
-    
-    } else if (userDivid >= computerDivid){
-        return `${false}  case of equality`;
+const compareGuesses = (randomGuess, userGuess, computerGuess) => {
+    randomGuess = generateTarget();
+    const userDistance = Math.abs(randomGuess - userGuess);
+    const computerDistance = Math.abs(randomGuess - computerGuess);
+    console.log(userDistance + ' human guess');
+    console.log(computerDistance + ' com gues');
+
+    if (userDistance < computerDistance) {
+        console.log("User is closer");
+        return true;  // Human wins
+    } else if (userDistance > computerDistance) {
+        console.log("Computer is closer");
+        return false;  // Computer wins
+    } else {
+        console.log("It's a tie, human wins");
+        return true;  // Tie goes to human
+    }
+};
+
+
+// testing the function generateTarget()
+// console.log(compareGuesses(generateTarget(), 4, 5));
+// console.log(compareGuesses(generateTarget(), 8, 4));
+// console.log(compareGuesses(5, 4, 4));
+
+// creating the Updating scroe functionnality
+
+const updateScore = (winner) =>{
+    if(winner === 'human'){
+        humanScore++;
+    }
+    else if(winner === 'computer'){
+        computerScore++;
+    }
+    else {
+        return 'Error Input'
     }
 }
 
-// testing the function generateTarget()
-console.log(compareGuesses(generateTarget(), 4, 5));
-console.log(compareGuesses(generateTarget(), 8, 4));
-console.log(compareGuesses(5, 4, 4));
+// test updateScore
+// updateScore('human');
+// console.log(humanScore);
+// updateScore('human');
+// console.log(humanScore);
+// updateScore('human');
+// console.log(humanScore);
+// updateScore('dummy');
+// console.log(humanScore);
+// updateScore('computer');
+// console.log(computerScore);
 
 
+const advanceRound = ()=>{
+    currentRoundNumber++;
+}
+// console.log(currentRoundNumber);
